3669541fdaa710f2ffa2030537dfb243e0ebf490
leticea <nascimento.leticeah@gmail.com>
2023-08-24 11:02:11 -0300

    feat(sidebar): creating sidebar

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,20 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ["./src/**/*.tsx"],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      gridTemplateColumns: {
+        app: "minmax(18rem, 20rem) 1fr",
+      },
+      maxWidth: {
+        app: "700px",
+      },
+      colors: {
+        leticia: "#8257e6",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
