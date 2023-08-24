864d5cd8a15108a37e11906c27525b29776862c3
leticea <nascimento.leticeah@gmail.com>
2023-08-24 11:11:53 -0300

    feat(sidebar): creating sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,3 +1,9 @@
+import { Logo } from "./Logo";
+
 export function Sidebar() {
-  return <aside className="border-r border-zinc-200 px-5 py-8">sidebar</aside>;
+  return (
+    <aside className="border-r border-zinc-200 px-5 py-8">
+      <Logo />
+    </aside>
+  );
 }
