a594715514b129327578d34ab6c9ad330ed78cc0
leticea <nascimento.leticeah@gmail.com>
2023-08-25 15:26:06 -0300

    feat(Sidebar): creating widget

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,4 +1,14 @@
-import { BarChart, CheckSquare, Flag, Home, Search, SquareStack, Users } from "lucide-react";
+import {
+  BarChart,
+  CheckSquare,
+  Cog,
+  Flag,
+  Home,
+  LifeBuoy,
+  Search,
+  SquareStack,
+  Users,
+} from "lucide-react";
 import { Logo } from "./Logo";
 import { NavItem } from "./NavItem";
 
@@ -26,9 +36,24 @@ export function Sidebar() {
 
       <div className="mt-auto flex flex-col gap-6">
         <nav className="space-y-0.5">
-          <NavItem title="Home" icon={Home} />
-          <NavItem title="Home" icon={Home} />
+          <NavItem title="Support" icon={LifeBuoy} />
+          <NavItem title="Settings" icon={Cog} />
         </nav>
+
+        <div className="flex flex-col gap-4 rounded-lg bg-violet-50 px-4 py-5">
+          <div className="space-y-1">
+            <span className="text-sm/5 font-medium text-violet-700">
+              Used space
+            </span>
+            <p className="text-sm/5 text-violet-500">
+              Your team has used 80% of your available space. Need more?
+            </p>
+          </div>
+
+          <div className="h-2 rounded-full bg-violet-100">
+            <div className="h-2 w-4/5 rounded-full bg-violet-600" />
+          </div>
+        </div>
       </div>
     </aside>
   );
