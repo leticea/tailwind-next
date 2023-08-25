938d59960a1eb785e34e6578a78056d44196b66e
leticea <nascimento.leticeah@gmail.com>
2023-08-25 16:23:40 -0300

    feat(Input): creating differents input component

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,13 +1,30 @@
 import { Search } from "lucide-react";
+import { ComponentProps } from "react";
 
-export function Input() {
+type InputPrefixProps = ComponentProps<"div">;
+
+export function InputPrefix(props: InputPrefixProps) {
+  return <div {...props} />;
+}
+
+type InputControlProps = ComponentProps<"input">;
+
+export function InputControl(props: InputControlProps) {
   return (
-    <div className="flex mx-1 w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm">
-        <Search className="h-5 w-5 text-zinc-500" />
-        <input
-          className="flex-1 w-40 border-0 outline-none bg-transparent p-0 text-zinc-900 placeholder-zinc-600"
-          placeholder="Search"
-        />
-      </div>
-  )
+    <input
+      className="flex-1 w-40 border-0 outline-none bg-transparent p-0 text-zinc-900 placeholder-zinc-600"
+      {...props}
+    />
+  );
+}
+
+export type InputRootProps = ComponentProps<"div">;
+
+export function InputRoot(props: InputRootProps) {
+  return (
+    <div
+      className="flex mx-1 w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm"
+      {...props}
+    ></div>
+  );
 }
