97daefb155f69667d59d6a4e8486a93f4396be15
leticea <nascimento.leticeah@gmail.com>
2023-08-28 14:41:18 -0300

    feat(page.tsx): form styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <SettingsTabs />
 
       <div className="mt-6 flex flex-col">
-        <div className="flex justify-between items-center">
+        <div className="flex justify-between items-center pb-5 border-b border-zinc-200">
           <div className="space-y-1">
             <h2 className="text-lg font-medium text-zinc-900">Personal Info</h2>
             <span className="text-sm text-zinc-500">
@@ -16,14 +16,25 @@ export default function Home() {
             </span>
           </div>
           <div className="flex items-center gap-2">
-            <button type="button">Cancel</button>
-            <button type="submit" form="settings">
+            <button
+              type="button"
+              className="rounded-lg px-4 py-2 text-sm font-semibold shadow-sm border border-zinc-300 text-zinc-700 hover:bg-zinc-50"
+            >
+              Cancel
+            </button>
+            <button
+              type="submit"
+              form="settings"
+              className="rounded-lg px-4 py-2 text-sm font-semibold shadow-sm bg-violet-600 text-white hover:bg-violet-700"
+            >
               Save
             </button>
           </div>
         </div>
 
-        <form id="settings" action=""></form>
+        <form id="settings" className="mt-6 flex flex-col w-full">
+          form
+        </form>
       </div>
     </>
   );
