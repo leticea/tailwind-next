971c48a047b18191ac07d037c1ce31c0fb8618c6
leticea <nascimento.leticeah@gmail.com>
2023-08-28 14:29:58 -0300

    feat(page.tsx): creating form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,25 @@ export default function Home() {
       <h1 className="text-3xl font-medium text-zinc-900">Settings</h1>
 
       <SettingsTabs />
+
+      <div className="mt-6 flex flex-col">
+        <div className="flex justify-between items-center">
+          <div className="space-y-1">
+            <h2 className="text-lg font-medium text-zinc-900">Personal Info</h2>
+            <span className="text-sm text-zinc-500">
+              Update photo and personal details here.
+            </span>
+          </div>
+          <div className="flex items-center gap-2">
+            <button type="button">Cancel</button>
+            <button type="submit" form="settings">
+              Save
+            </button>
+          </div>
+        </div>
+
+        <form id="settings" action=""></form>
+      </div>
     </>
   );
 }
