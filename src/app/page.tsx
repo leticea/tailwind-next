1253a8730b433d22a5961798a0d6d37df0fb34cd
leticea <nascimento.leticeah@gmail.com>
2023-08-28 13:30:56 -0300

    feat(SettingsTabs): creating tabs component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
-import * as Tabs from "@radix-ui/react-tabs";
+import { SettingsTabs } from "@/components/SettingsTabs";
 
 export default function Home() {
   return (
     <>
       <h1 className="text-3xl font-medium text-zinc-900">Settings</h1>
 
-      <Tabs.Root>
-        <Tabs.List>
-          
-        </Tabs.List>
-      </Tabs.Root>
+      <SettingsTabs />
     </>
   );
 }
