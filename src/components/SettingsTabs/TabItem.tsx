e48c81b480ba0082ec2e2f46378d1c7ad0e5e933
leticea <nascimento.leticeah@gmail.com>
2023-08-28 13:46:08 -0300

    feat(TabItem): tabs trigger styles

--- src/components/SettingsTabs/TabItem.tsx
+++ src/components/SettingsTabs/TabItem.tsx
@@ -12,9 +12,13 @@ export function TabItem({ value, title, isSelected = false }: TabItemProps) {
   return (
     <Tabs.Trigger
       value={value}
-      className="px-1 pb-4 text-sm font-medium text-zinc-500 hover:text-violet-700"
+      className="relative px-1 pb-4 text-sm font-medium text-zinc-500 hover:text-violet-700"
     >
       <span>{title}</span>
+
+      {isSelected && (
+        <div className="absolute left-0 right-0 h-0.5 bg-violet-700 -bottom-px" />
+      )}
     </Tabs.Trigger>
   );
 }
--- src/components/SettingsTabs/index.tsx
+++ src/components/SettingsTabs/index.tsx
@@ -1,12 +1,22 @@
 "use client";
 
 import * as Tabs from "@radix-ui/react-tabs";
+import { TabItem } from "./TabItem";
 
 export function SettingsTabs() {
   return (
     <Tabs.Root>
       <Tabs.List className="mt-6 flex w-full items-center gap-4 border-b border-zinc-200">
-        
+        <TabItem value="tab1" title="My details" isSelected />
+        <TabItem value="tab2" title="Profile" />
+        <TabItem value="tab3" title="Password" />
+        <TabItem value="tab4" title="Team" />
+        <TabItem value="tab5" title="Plan" />
+        <TabItem value="tab6" title="Billing" />
+        <TabItem value="tab7" title="Email" />
+        <TabItem value="tab8" title="Notifications" />
+        <TabItem value="tab9" title="Integrations" />
+        <TabItem value="tab10" title="API" />
       </Tabs.List>
     </Tabs.Root>
   );
