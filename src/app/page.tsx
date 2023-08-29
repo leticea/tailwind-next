d1f9bb68586cc22bb024e682e2b69f97b6918108
leticea <nascimento.leticeah@gmail.com>
2023-08-29 14:46:51 -0300

    feat(page.tsx): creating form input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,7 +34,10 @@ export default function Home() {
           </div>
         </div>
 
-        <form id="settings" className="mt-6 flex flex-col w-full gap-5">
+        <form
+          id="settings"
+          className="mt-6 flex flex-col w-full gap-5 divide-y divide-zinc-200"
+        >
           <div className="grid gap-3 grid-cols-form">
             <label
               htmlFor="firstName"
@@ -53,7 +56,7 @@ export default function Home() {
             </div>
           </div>
 
-          <div className="grid gap-3 grid-cols-form">
+          <div className="grid gap-3 grid-cols-form pt-5">
             <label
               htmlFor="email"
               className="text-sm font-medium text-zinc-700"
@@ -61,15 +64,95 @@ export default function Home() {
               Email address
             </label>
             <InputRoot>
-            <InputPrefix>
-              <Mail className="h-5 w-5 text-zinc-500" />
-            </InputPrefix>
+              <InputPrefix>
+                <Mail className="h-5 w-5 text-zinc-500" />
+              </InputPrefix>
               <InputControl
                 id="email"
                 type="email"
                 defaultValue="leticia@leticia.com.br"
               />
             </InputRoot>
+          </div>
+
+          <div className="grid gap-3 grid-cols-form pt-5">
+            <label
+              htmlFor="email"
+              className="text-sm font-medium text-zinc-700"
+            >
+              Your photo
+              <span className="text-sm font-normal text-zinc-500 block mt-0.5">
+                This will be displayed on your profile.
+              </span>
+            </label>
+            <div></div>
+          </div>
+
+          <div className="grid gap-3 grid-cols-form pt-5">
+            <label htmlFor="role" className="text-sm font-medium text-zinc-700">
+              Role
+            </label>
+            <InputRoot>
+              <InputControl id="role" defaultValue="Student" />
+            </InputRoot>
+          </div>
+
+          <div className="grid gap-3 grid-cols-form pt-5">
+            <label
+              htmlFor="country"
+              className="text-sm font-medium text-zinc-700"
+            >
+              Country
+            </label>
+            <div></div>
+          </div>
+
+          <div className="grid gap-3 grid-cols-form pt-5">
+            <label
+              htmlFor="timezone"
+              className="text-sm font-medium text-zinc-700"
+            >
+              Timezone
+            </label>
+            <div></div>
+          </div>
+
+          <div className="grid gap-3 grid-cols-form pt-5">
+            <label htmlFor="bio" className="text-sm font-medium text-zinc-700">
+              Bio
+              <span className="text-sm font-normal text-zinc-500 block mt-0.5">
+                Write a short introduction.
+              </span>
+            </label>
+            <div></div>
+          </div>
+
+          <div className="grid gap-3 grid-cols-form pt-5">
+            <label
+              htmlFor="projects"
+              className="text-sm font-medium text-zinc-700"
+            >
+              Portfolio projects
+              <span className="text-sm font-normal text-zinc-500 block mt-0.5">
+                Share a few snippets of your work.
+              </span>
+            </label>
+            <div></div>
+          </div>
+
+          <div className="flex items-center justify-end gap-2 pt-5">
+          <button
+              type="button"
+              className="rounded-lg px-4 py-2 text-sm font-semibold shadow-sm border border-zinc-300 text-zinc-700 hover:bg-zinc-50"
+            >
+              Cancel
+            </button>
+            <button
+              type="submit"
+              className="rounded-lg px-4 py-2 text-sm font-semibold shadow-sm bg-violet-600 text-white hover:bg-violet-700"
+            >
+              Save
+            </button>
           </div>
         </form>
       </div>
