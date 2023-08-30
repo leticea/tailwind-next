b0671be1e9cecd4417240c51e6b6c3c06121c26d
leticea <nascimento.leticeah@gmail.com>
2023-08-30 14:03:02 -0300

    feat(page.tsx): select file styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -92,10 +92,10 @@ export default function Home() {
 
               <label
                 htmlFor="photo"
-                className="flex-1 cursor-pointer flex flex-col items-center gap-3 rounded-lg border border-zinc-300 px-6 py-4 text-center text-zinc-500 shadow-sm"
+                className="group flex-1 cursor-pointer flex flex-col items-center gap-3 rounded-lg border border-zinc-300 px-6 py-4 text-center text-zinc-500 shadow-sm hover:border-violet-200 hover:bg-violet-25 hover:text-violet-500"
               >
-                <div className="rounded-full border-6 border-zinc-50 bg-zinc-100 p-2">
-                  <UploadCloud className="h-5 w-5 text-zinc-600" />
+                <div className="rounded-full border-6 border-zinc-50 bg-zinc-100 p-2 group-hover:border-violet-50 group-hover:bg-violet-100">
+                  <UploadCloud className="h-5 w-5 text-zinc-600 group-hover:text-violet-600" />
                 </div>
 
                 <div className="flex flex-col items-center gap-1">
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,11 @@ const config: Config = {
       borderWidth: {
         6: "6px",
       },
+      colors: {
+        violet: {
+          25: "#fcfaff",
+        },
+      },
     },
   },
   plugins: [],
