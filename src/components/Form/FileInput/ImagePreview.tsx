6856b9835bed06a284f3b9ee34d47f6461a66eac
leticea <nascimento.leticeah@gmail.com>
2023-08-31 15:26:30 -0300

    feat(FileInput): creating file input components

--- src/components/Form/FileInput/ImagePreview.tsx
+++ src/components/Form/FileInput/ImagePreview.tsx
@@ -1,5 +1,9 @@
-export interface ImagePreviewProps {}
+import { User } from "lucide-react";
 
-export function ImagePreview(props: ImagePreviewProps) {
-  return <div />;
+export function ImagePreview() {
+  return (
+    <div className="bg-violet-50 flex h-16 w-16 items-center justify-center rounded-full">
+      <User className="h-8 w-8 text-violet-500" />
+    </div>
+  );
 }
