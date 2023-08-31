2bf8488a6617038cbd3c803166fd5dd69bff24ee
leticea <nascimento.leticeah@gmail.com>
2023-08-31 15:21:59 -0300

    feat(FileInput): creating file input components

---
+++ src/components/Form/FileInput/ImagePreview.tsx
@@ -0,0 +1,5 @@
+export interface ImagePreviewProps {}
+
+export function ImagePreview(props: ImagePreviewProps) {
+  return <div />;
+}
---
+++ src/components/Form/FileInput/Trigger.tsx
@@ -0,0 +1,5 @@
+export interface TriggerProps {}
+
+export function Trigger(props: TriggerProps) {
+  return <div />;
+}
