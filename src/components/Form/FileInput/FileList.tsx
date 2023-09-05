217063158e77bec04644a520c43396fbff28fd72
leticea <nascimento.leticeah@gmail.com>
2023-09-05 19:49:04 -0300

    feat(FileList): creating fileList component

---
+++ src/components/Form/FileInput/FileList.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import { useFileInput } from "./Root";
+
+export function FileList() {
+  const { files } = useFileInput();
+  return <div className="mt-4 space-y-3">
+    
+  </div>;
+}
