4f396d658a575df26b8c2e04b39fd5a46cd8e625
leticea <nascimento.leticeah@gmail.com>
2023-09-05 19:53:52 -0300

    feat(FileList): creating fileList component

--- src/components/Form/FileInput/FileList.tsx
+++ src/components/Form/FileInput/FileList.tsx
@@ -4,7 +4,11 @@ import { useFileInput } from "./Root";
 
 export function FileList() {
   const { files } = useFileInput();
-  return <div className="mt-4 space-y-3">
-    
-  </div>;
+  return (
+    <div className="mt-4 space-y-3">
+      {files.map((file) => {
+        return <p key={file.name}>{file.name}</p>;
+      })}
+    </div>
+  );
 }
