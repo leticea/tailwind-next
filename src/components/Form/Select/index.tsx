e366020739fddf2054ea4fd038078a539cb3c7e4
leticea <nascimento.leticeah@gmail.com>
2023-09-17 12:07:58 -0300

    feat(SelectItem): creating SelectItem function

--- src/components/Form/Select/index.tsx
+++ src/components/Form/Select/index.tsx
@@ -1,16 +1,20 @@
 "use client";
 
 import * as SelectPrimitive from "@radix-ui/react-select";
-import { Check, ChevronDown } from "lucide-react";
+import { ChevronDown } from "lucide-react";
+import { ReactNode } from "react";
 
-export interface SelectProps {}
+export interface SelectProps {
+  children: ReactNode;
+  placeholder: string;
+}
 
-export function Select(props: SelectProps) {
+export function Select({ children, placeholder }: SelectProps) {
   return (
     <SelectPrimitive.Root>
       <SelectPrimitive.Trigger className="flex h-11 w-full items-center justify-between gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm data-[placeholder]:text-zinc-600">
         <SelectPrimitive.Value
-          placeholder="Select a country..."
+          placeholder={placeholder}
           className="text-black"
         />
         <SelectPrimitive.Icon>
@@ -25,22 +29,7 @@ export function Select(props: SelectProps) {
           sideOffset={8}
           className="z-10 rounded-lg border border-zinc-200 bg-white w-[--radix-select-trigger-width] overflow-hidden"
         >
-          <SelectPrimitive.Viewport>
-            
-
-            <SelectPrimitive.Item
-              value="us"
-              className="flex items-center justify-between gap-2 px-3 py-2.5 outline-none data-[highlighted]:bg-zinc-100"
-            >
-              <SelectPrimitive.ItemText className="text-black">
-                United States
-              </SelectPrimitive.ItemText>
-
-              <SelectPrimitive.ItemIndicator>
-                <Check className="h-4 w-4 text-violet-500" />
-              </SelectPrimitive.ItemIndicator>
-            </SelectPrimitive.Item>
-          </SelectPrimitive.Viewport>
+          <SelectPrimitive.Viewport>{children}</SelectPrimitive.Viewport>
         </SelectPrimitive.Content>
       </SelectPrimitive.Portal>
     </SelectPrimitive.Root>
