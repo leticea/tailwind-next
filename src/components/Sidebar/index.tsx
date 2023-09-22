f478bbc68c1371fc2494fe3cc3ab395163134df4
leticea <nascimento.leticeah@gmail.com>
2023-09-22 19:28:09 -0300

    feat(Sidebar): creating responsive layout

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -17,7 +17,7 @@ import { InputControl, InputPrefix, InputRoot } from "../Input";
 
 export function Sidebar() {
   return (
-    <aside className="border-zinc-200 border-b flex flex-col gap-6 p-4 fixed left-0 top-0 right-0 z-20 bg-white lg:right-auto lg:w-80 lg:border-r lg:px-5 lg:py-8">
+    <aside className="border-zinc-200 border-b bottom-0 flex flex-col gap-6 p-4 fixed left-0 top-0 right-0 z-20 bg-white lg:right-auto lg:w-80 lg:border-r lg:px-5 lg:py-8 lg:relative">
       <Logo />
 
       <InputRoot>
