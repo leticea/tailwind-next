71c15f3b8285724087ba151561540b9300bad6c4
leticea <nascimento.leticeah@gmail.com>
2023-09-22 17:23:09 -0300

    feat(FileItem): creating FileItem component

--- src/components/Form/FileInput/FileList.tsx
+++ src/components/Form/FileInput/FileList.tsx
@@ -1,10 +1,8 @@
 "use client";
 
-import { Trash2, UploadCloud } from "lucide-react";
 import { useFileInput } from "./Root";
-import { formatBytes } from "@/utils/format-bytes";
 import { useAutoAnimate } from "@formkit/auto-animate/react";
-import { Button } from "@/components/Button";
+import { FileItem } from "./FileItem";
 
 export function FileList() {
   const { files } = useFileInput();
@@ -13,9 +11,7 @@ export function FileList() {
   return (
     <div ref={parent} className="mt-4 space-y-3">
       {files.map((file) => {
-        return (
-          
-        );
+        return <FileItem key={file.name} name={file.name} size={file.size} />;
       })}
     </div>
   );
