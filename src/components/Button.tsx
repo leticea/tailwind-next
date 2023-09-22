4b40daafb1fb63a76bfcded2af8b2dbcdf6c1563
leticea <nascimento.leticeah@gmail.com>
2023-09-22 15:09:13 -0300

    feat(Button): creating button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,11 @@
+export function Button() {
+  return (
+    <button
+      type="submit"
+      form="settings"
+      className="rounded-lg px-4 py-2 text-sm font-semibold shadow-sm bg-violet-600 text-white hover:bg-violet-700"
+    >
+      Save
+    </button>
+  );
+}
