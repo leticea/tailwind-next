d957b245085c5ea6aaefbc810f77b435a9f0cae0
leticea <nascimento.leticeah@gmail.com>
2023-09-22 17:07:23 -0300

    feat(Button): creating ghost variant

--- src/components/Sidebar/Profile.tsx
+++ src/components/Sidebar/Profile.tsx
@@ -1,4 +1,5 @@
 import { LogOut } from "lucide-react";
+import { Button } from "../Button";
 
 export function Profile() {
   return (
@@ -16,12 +17,9 @@ export function Profile() {
         <span className="text-sm text-zinc-500">leticia@leticia.com.br</span>
       </div>
 
-      <button
-        type="button"
-        className="ml-auto p-2 hover:bg-zinc-100 rounded-md"
-      >
+      <Button type="button" variant="ghost">
         <LogOut className="h-5 w-5 text-zinc-500" />
-      </button>
+      </Button>
     </div>
   );
 }
