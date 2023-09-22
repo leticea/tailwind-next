41d5b0cf9514fe74bf142010e8938a2522b1b06e
leticea <nascimento.leticeah@gmail.com>
2023-09-22 17:51:24 -0300

    feat(FileItem): creating error state

--- src/components/Form/FileInput/FileItem.tsx
+++ src/components/Form/FileInput/FileItem.tsx
@@ -8,7 +8,7 @@ export interface FileItemProps {
 }
 
 export function FileItem({ name, size }: FileItemProps) {
-  const state = "complete";
+  const state = "error" as "progress" | "error" | "complete";
 
   return (
     <div className="group flex items-start gap-4 rounded-lg border border-zinc-200 p-4">
@@ -16,24 +16,42 @@ export function FileItem({ name, size }: FileItemProps) {
         <UploadCloud className="h-4 w-4" />
       </div>
 
-      <div className="flex flex-1 flex-col items-start gap-1">
-        <div className="flex flex-col">
-          <span className="text-sm font-medium text-zinc-700">{name}</span>
-          <span className="text-sm text-zinc-500">{formatBytes(size)}</span>
-        </div>
-
-        <div className="flex w-full items-center gap-3">
-          <div className="h-2 flex-1 rounded-full bg-zinc-100">
-            <div
-              className="h-2 rounded-full bg-violet-600"
-              style={{ width: state === "complete" ? "100%" : "80%" }}
-            />
+      {state === "error" ? (
+        <div className="flex flex-1 flex-col items-start gap-1">
+          <div className="flex flex-col">
+            <span className="text-sm font-medium text-red-700">
+              Upload failed, please try again.
+            </span>
+            <span className="text-sm text-red-600">{name}</span>
           </div>
-          <span className="text-sm font-medium text-zinc-700">
-            {state === "complete" ? "100%" : "80%"}
-          </span>
+
+          <button
+            type="button"
+            className="text-sm font-semibold text-red-700 hover:text-red-900"
+          >
+            Try again
+          </button>
         </div>
-      </div>
+      ) : (
+        <div className="flex flex-1 flex-col items-start gap-1">
+          <div className="flex flex-col">
+            <span className="text-sm font-medium text-zinc-700">{name}</span>
+            <span className="text-sm text-zinc-500">{formatBytes(size)}</span>
+          </div>
+
+          <div className="flex w-full items-center gap-3">
+            <div className="h-2 flex-1 rounded-full bg-zinc-100">
+              <div
+                className="h-2 rounded-full bg-violet-600"
+                style={{ width: state === "complete" ? "100%" : "80%" }}
+              />
+            </div>
+            <span className="text-sm font-medium text-zinc-700">
+              {state === "complete" ? "100%" : "80%"}
+            </span>
+          </div>
+        </div>
+      )}
 
       {state === "complete" ? (
         <CheckCircle2 className="h-5 w-5 fill-violet-600 text-white" />
