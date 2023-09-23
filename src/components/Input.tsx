b24a039419c6d93ef3e646a4ee609ccc0c283aa1
leticea <nascimento.leticeah@gmail.com>
2023-09-23 17:42:41 -0300

    feat(Input): inserting dark mode

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -12,7 +12,7 @@ type InputControlProps = ComponentProps<"input">;
 export function InputControl(props: InputControlProps) {
   return (
     <input
-      className="flex-1 border-0 outline-none bg-transparent p-0 text-zinc-900 placeholder-zinc-600"
+      className="flex-1 border-0 outline-none bg-transparent p-0 text-zinc-900 placeholder-zinc-600 dark:placeholder-zinc-400 dark:text-zinc-100"
       {...props}
     />
   );
