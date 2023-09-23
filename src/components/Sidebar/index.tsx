58dc4e92c9bae369b53f9092bd81cdd7aac39a65
leticea <nascimento.leticeah@gmail.com>
2023-09-23 08:14:23 -0300

    feat(Sidebar): inserting radix-ui-collapsible

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -14,40 +14,43 @@ import { NavItem } from "./NavItem";
 import { UsedSpaceWidget } from "./UsedSpaceWidget";
 import { Profile } from "./Profile";
 import { InputControl, InputPrefix, InputRoot } from "../Input";
+import * as Collapsible from "@radix-ui/react-collapsible";
 
 export function Sidebar() {
   return (
-    <aside className="border-zinc-200 border-b bottom-0 flex flex-col gap-6 p-4 fixed left-0 top-0 right-0 z-20 bg-white lg:right-auto lg:w-80 lg:border-r lg:px-5 lg:py-8">
+    <Collapsible.Root className="border-zinc-200 border-b bottom-0 flex flex-col gap-6 p-4 fixed left-0 top-0 right-0 z-20 bg-white lg:right-auto lg:w-80 lg:border-r lg:px-5 lg:py-8">
       <Logo />
 
-      <InputRoot>
-        <InputPrefix>
-          <Search className="h-5 w-5 text-zinc-500" />
-        </InputPrefix>
-        <InputControl placeholder="Search" />
-      </InputRoot>
+      <Collapsible.Content>
+        <InputRoot>
+          <InputPrefix>
+            <Search className="h-5 w-5 text-zinc-500" />
+          </InputPrefix>
+          <InputControl placeholder="Search" />
+        </InputRoot>
 
-      <nav className="space-y-0.5">
-        <NavItem title="Home" icon={Home} />
-        <NavItem title="Dashboard" icon={BarChart} />
-        <NavItem title="Projects" icon={SquareStack} />
-        <NavItem title="Tasks" icon={CheckSquare} />
-        <NavItem title="Reporting" icon={Flag} />
-        <NavItem title="Users" icon={Users} />
-      </nav>
-
-      <div className="mt-auto flex flex-col gap-6">
         <nav className="space-y-0.5">
-          <NavItem title="Support" icon={LifeBuoy} />
-          <NavItem title="Settings" icon={Cog} />
+          <NavItem title="Home" icon={Home} />
+          <NavItem title="Dashboard" icon={BarChart} />
+          <NavItem title="Projects" icon={SquareStack} />
+          <NavItem title="Tasks" icon={CheckSquare} />
+          <NavItem title="Reporting" icon={Flag} />
+          <NavItem title="Users" icon={Users} />
         </nav>
 
-        <UsedSpaceWidget />
+        <div className="mt-auto flex flex-col gap-6">
+          <nav className="space-y-0.5">
+            <NavItem title="Support" icon={LifeBuoy} />
+            <NavItem title="Settings" icon={Cog} />
+          </nav>
 
-        <div className="h-px bg-zinc-200" />
+          <UsedSpaceWidget />
 
-        <Profile />
-      </div>
-    </aside>
+          <div className="h-px bg-zinc-200" />
+
+          <Profile />
+        </div>
+      </Collapsible.Content>
+    </Collapsible.Root>
   );
 }
