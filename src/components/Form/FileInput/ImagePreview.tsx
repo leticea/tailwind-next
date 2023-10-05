f95c2d59dc514f761918c5c02a6892ed8c2ff5fe
leticea <nascimento.leticeah@gmail.com>
2023-10-05 19:27:47 -0300

    dark styles

--- src/components/Form/FileInput/ImagePreview.tsx
+++ src/components/Form/FileInput/ImagePreview.tsx
@@ -17,8 +17,8 @@ export function ImagePreview() {
 
   if (previewURL === null) {
     return (
-      <div className="bg-violet-50 flex h-16 w-16 items-center justify-center rounded-full">
-        <User className="h-8 w-8 text-violet-500" />
+      <div className="bg-violet-50 flex h-16 w-16 items-center justify-center rounded-full dark:bg-violet-500/10">
+        <User className="h-8 w-8 text-violet-500 dark:text-violet-300" />
       </div>
     );
   } else {
