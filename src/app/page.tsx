cc37385ffb460c27a7ba41c3c3bac88229e74656
leticea <nascimento.leticeah@gmail.com>
2023-10-05 18:46:45 -0300

    dark styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,9 @@ import { Button } from "@/components/Button";
 export default function Home() {
   return (
     <>
-      <h1 className="text-3xl font-medium text-zinc-900">Settings</h1>
+      <h1 className="text-3xl font-medium text-zinc-900 dark:text-zinc-100">
+        Settings
+      </h1>
       <SettingsTabs />
 
       <div className="mt-6 flex flex-col">
